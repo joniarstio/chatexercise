8b46aa1d5b889a3a9e3d8e17aef145de8dcb03f7
Joni Arstio <joni.arstio@gmail.com>
2021-04-01 13:56:11 +0300

    initial commit

---
+++ app/src/main/java/com/example/chatexercise/Settings.kt
@@ -0,0 +1,44 @@
+package com.example.chatexercise
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class Settings : AppCompatActivity() {
+
+    private lateinit var email: TextView
+    private lateinit var logoutButton: Button
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        email = findViewById(R.id.email)
+        logoutButton = findViewById(R.id.logoutButton)
+
+        supportActionBar?.apply {
+            title="Settings"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        val currentUser = intent.getParcelableExtra<FirebaseUser>("currentUser")
+        email.text = currentUser?.email
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    fun signOut(view: View) {
+        FirebaseAuth.getInstance().signOut()
+        email.text = ""
+    }
+}
